b61538241dd39ff3f98c733462f47a0311df7b33
Arcangelo Pisa <arcangelopisa@me.com>
2021-03-24 17:07:45 +0100

    Initial Commit

---
+++ NeatoTest/NeatoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeatoTestApp.swift
+//  NeatoTest
+//
+//  Created by Arcangelo Pisa on 24/03/21.
+//
+
+import SwiftUI
+
+@main
+struct NeatoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
